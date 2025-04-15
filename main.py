5e26500a8a422f1bf3995f763c421e26bbde2c6a
argonautgeo <barsik030@bk.ru>
2025-04-15 16:23:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from aiogram import Bot, Dispatcher
-
 import asyncio
+import logging
+import sys
 
-bot = Bot(token='7930467714:AAGnJixu-zm1bpe3MK90o4DeeALRHiTQXsM')
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+
+from src.config import conf
+from src.payments.handlers import router as payment_router
+from src.users.handlers import router as user_router
+
+routers = [user_router, payment_router]
 dp = Dispatcher(bot=bot)
+dp.include_routers(*routers)
+bot = Bot(token='7930467714:AAGnJixu-zm1bpe3MK90o4DeeALRHiTQXsM')
 
-async def main():
-    from handlers import dp
+if __name__ == "main":
+    # Включаем логирование.
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    # Запускаем поток.
     try:
-        await dp.start_polling()
-    finally:
-        await bot.session.close()
-
-if __name__ == "__main__":
-    try:
-        asyncio.run(main())
+        asyncio.run(dp.start_polling(bot))
     except (KeyboardInterrupt, SystemExit):
-        print('Bot stopped!')
+        logging.info("Bot stopped")
