a05022e4cab1f5e4fd71a0fac462f39ccd3020c4
argonautgeo <barsik030@bk.ru>
2025-04-15 16:27:45 +0800

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,11 +1,15 @@
-from aiogram.types import WebAppInfo
-from aiogram import types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types.web_app_info import WebAppInfo
 
-web_app = WebAppInfo(url='site_url')
+from src.config import conf
 
-keyboard = types.ReplyKeyboardMarkup(
-    keyboard=[
-        [types.KeyboardButton(text='Site', web_app=web_app)]
-    ],
-    resize_keyboard=True
+main_kb = [
+    [KeyboardButton(text="Сделать заказ", web_app=WebAppInfo(url=conf.webapp_url))]
+]
+
+reply_main_kb = ReplyKeyboardMarkup(
+    keyboard=main_kb,
+    # Чтобы прилично выглядеть на телефоне.
+    resize_keyboard=True,
+    input_field_placeholder="Выберите пункт меню",
 )
