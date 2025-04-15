6c13f29edc4754b518c7a9f2ebcc8210338a3eea
argonautgeo <barsik030@bk.ru>
2025-04-15 16:15:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,91 +1,45 @@
-let tg = window.Telegram.WebApp;
+var tg = window.Telegram.WebApp;
 
+// Расширяем на весь экран.
 tg.expand();
 
+// Кнопка просмотра заказа (реализуется тг - не сайтом).
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
+// Хранение выбранного товара.
 let item = "";
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
+let prices = new Object();
+prices[1] = 220;
+prices[2] = 460;
+prices[3] = 240;
+prices[4] = 190;
+prices[5] = 100;
+prices[6] = 150;
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
-});
+for (var i = 1; i <= 6; i++) {
+	let btn = document.getElementById("btn" + i.toString());
+	btn.btn_id = i;
+	btn.price = prices[i];
+	btn.addEventListener("click", function() {
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide();
+		}
+		else {
+			tg.MainButton.setText("Перейти к оплате (" + btn.price.toString() + "₽)");
+			item = this.btn_id.toString();
+			tg.MainButton.show();
+		}
+	});
+}
 
-btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
-		item = "2";
-		tg.MainButton.show();
-	}
-});
-
-btn3.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
-		item = "3";
-		tg.MainButton.show();
-	}
-});
-
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
-		item = "4";
-		tg.MainButton.show();
-	}
-});
-
-btn5.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
-		item = "5";
-		tg.MainButton.show();
-	}
-});
-
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
-});
-
-
+// Отправка данных в тг.
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
 });
 
-
+// Добавление в usercard данных из тг.
 let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
@@ -94,7 +48,6 @@ p.innerText = `${tg.initDataUnsafe.user.first_name}
 ${tg.initDataUnsafe.user.last_name}`;
 
 usercard.appendChild(p);
-
 
 
 
